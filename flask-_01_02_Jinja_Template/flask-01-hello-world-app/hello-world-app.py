d3450776823ded081afb2a622a250334bc8d4b14
aman <aman@aman.com>
2025-06-26 21:42:21 -0400

    intial commit

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,12 +1,15 @@
-from flask import Flask 
+from flask import Flask
 app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Hello Simret"
 
-
-
-
-
-
+@app.route("/Bye")
+def theend():
+    return "<h1>Good Bye Simmy</h1>"
 if __name__ == '__main__':
 
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    app.run()
+
+    # app.run(debug=True, port=30000)
+    # # app.run(host= '0.0.0.0', port=8081)
--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -2,13 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Welcome"
 
 
 
+@app.route("/head")
+def head():
+    return render_template("index.html", number1 = 10, number2 = 20)  
 
 
+@app.route("/sum")
+def sum():
+
+    val1 = 22
+    val2 = 36
+    mysum = val1 + val2
+    return render_template("body.html", value1 = val1, value2 = val2, sum = mysum)
 
 
 if __name__== "__main__":
-    app.run(debug=True, port=30000)
-    # app.run(host= '0.0.0.0', port=8081)
+    app.run(host="0.0.0.0")
+    
+
