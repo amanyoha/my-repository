8c9db956e83e20bcbbbb60f9f87b49b867e28fc2
aman <aman@aman.com>
2025-07-11 19:16:07 -0400

    add Kittens push

--- flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -2,11 +2,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
-
-
+@app.route("/")
+def home():
+    return "Hello Amanuel" 
+@app.route("/bye")
+def theend():
+    return "<h1>The End</h1>"
 
 if __name__ == '__main__':
+    app.run(port=8080)
 
-    app.run(debug=True, port=30000)
+    # app.run(debug=True, port=30000)
     # app.run(host= '0.0.0.0', port=8081)
--- flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -2,13 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Welcome to Home"
 
+@app.route("/head")
+def head():
+    return render_template("index.html", number1 = 100, number2 = 500)
 
-
-
-
-
+@app.route("/sum")
+def sum():
+    val1 = 40 
+    val2 = 50 
+    mysum = val1 + val2
+    return render_template ("body.html", value1 = val1 , value2 = val2, sum = mysum)
 
 if __name__== "__main__":
-    app.run(debug=True, port=30000)
+    app.run(host="0.0.0.0")
+    #app.run(debug=True, port=30000)
     # app.run(host= '0.0.0.0', port=8081)
